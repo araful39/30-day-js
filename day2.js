21dfaece5ebba01878984cc12cc69f5c51805515
araful <rajuslam39@gmail.com>
2026-04-24 01:03:22 +0600

    day2 done

---
+++ day2.js
@@ -0,0 +1,21 @@
+
+// var name ="raje";
+//  var name ="Raju";
+
+// let city = "Dhaka" ;
+//  city = "Rangpur";
+
+
+//  const country = "Bangladesh" ;
+
+let userName ="Raju";
+
+userName ="Rahim";
+
+const pi =3.14;
+
+pi=3.16;
+
+console.log(userName);
+
+console.log(pi);
