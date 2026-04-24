716c5ed423cf4f4e6f469fc51c5b00ef4d3fe4d5
araful <rajuslam39@gmail.com>
2026-04-24 23:44:40 +0600

    3

---
+++ day3.js
@@ -0,0 +1,40 @@
+
+// var name ="raje";
+//  var name ="Raju";
+
+// let city = "Dhaka" ;
+//  city = "Rangpur";
+
+
+//  const country = "Bangladesh" ;
+
+// let userName ="Raju";
+
+// userName ="Rahim";
+
+// const pi =3.14;
+
+// pi=3.16;
+
+// console.log(userName);
+
+// console.log(pi);
+
+
+let age =28;
+
+let name ="Dhaka";
+
+let isLogin=true; //false
+
+let store=[ 20, false, "Dhaka", "Rangpur"];
+
+console.log(store[2]);
+console.log(store[1]);
+
+
+let ob = {
+    name:"Raju"
+};
+// data type
+console.log(ob["name"]);
