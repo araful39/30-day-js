07cac71c7e90c493d7804770baf9fbc303937ede
araful <rajuslam39@gmail.com>
2026-04-25 00:05:22 +0600

    day4 operators done

---
+++ day4.js
@@ -0,0 +1,29 @@
+// let a =10;
+// let b =5;
+
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+
+// console.log(a%b);
+// console.log(a**b);
+
+
+// let x =10;
+// let y ="10";
+
+// console.log(x==y);
+// console.log(x===y);
+
+
+// let age =20;
+// let hasId=true;
+
+// console.log(age >21 && hasId);
+
+let num = 10;
+ 
+num += 50;
+
+console.log(num);
