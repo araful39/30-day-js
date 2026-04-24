705cb7d483a6378cf72046e8dcd3f416da804c82
araful <rajuslam39@gmail.com>
2026-04-24 23:42:18 +0600

    day3 done

--- day2.js
+++ day2.js
@@ -8,14 +8,33 @@
 
 //  const country = "Bangladesh" ;
 
-let userName ="Raju";
+// let userName ="Raju";
 
-userName ="Rahim";
+// userName ="Rahim";
 
-const pi =3.14;
+// const pi =3.14;
 
-pi=3.16;
+// pi=3.16;
 
-console.log(userName);
+// console.log(userName);
 
-console.log(pi);
+// console.log(pi);
+
+
+let age =28;
+
+let name ="Dhaka";
+
+let isLogin=true; //false
+
+let store=[ 20, false, "Dhaka", "Rangpur"];
+
+console.log(store[2]);
+console.log(store[1]);
+
+
+let ob = {
+    name:"Raju"
+};
+
+console.log(ob["name"]);
