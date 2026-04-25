e0fdd9679c9bb540caee09ce112d0e0d30090f12
araful <rajuslam39@gmail.com>
2026-04-26 01:49:23 +0600

    feat(js): add Numbers A to Z tutorial in Bangla
    - Covered number basics (int, float, negative)
    - Explained NaN and Infinity
    - Added number methods (toFixed, parseInt, etc.)
    - Included Math object examples
    - Covered type conversion & coercion
    - Added BigInt and interview examples

---
+++ day6.js
@@ -0,0 +1,72 @@
+// let a =10;
+
+
+// let b =10.5;
+
+// let c =-5;
+
+
+// let x =1e6;
+
+// console.log(x);
+
+// console.log(0.1 +0.2);
+
+// console.log("abc" /2);
+
+// console.log(isNaN("abc"));
+
+// console.log(isNaN("123"));
+
+// console.log(1/0);
+
+
+
+// let num =100;
+// console.log(num.toString());
+
+
+// let num1=5.6789;
+
+// console.log(num1.toFixed(2));
+
+
+// let  num= Number("10");
+
+// console.log(  num);
+// console.log( typeof num);
+
+// let num = parseInt("10.5");
+// console.log(num);
+
+// let num = parseFloat("10.5");
+// console.log(num);
+
+// let num = Math.round(4.5);
+
+// console.log(num);
+
+// let num = Math.ceil(4.5);
+
+// console.log(num);
+
+// let random=Math.random();
+
+// console.log(random);
+
+// let random =Math.floor (Math.random() *10);
+
+// console.log(random);
+
+
+// let num =Math.max(10, 20, 15);
+// let num =Math.min(10, 20, 15);
+
+// console.log(num);
+
+// let num =Number("10");
+let num =Number(false);
+
+console.log(num);
+
+
