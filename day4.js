878d526915bae4c83118fcb69d4cd7db378c446a
araful <rajuslam39@gmail.com>
2026-04-26 01:30:17 +0600

    day 5 ,  string a to z all methods done

--- day4.js
+++ day4.js
@@ -22,8 +22,85 @@
 
 // console.log(age >21 && hasId);
 
-let num = 10;
+// let num = 10;
  
-num += 50;
+// num += 50;
 
-console.log(num);
+// console.log(num);
+
+
+// primitve types
+
+// String, 
+// Number,
+// Boolean,
+// undefined,
+// null,
+// Symbol
+
+// non primitve types
+
+// Object,
+// Array, Function
+
+
+// let a =10;
+// let b =a;
+
+// b = 20;
+
+// // console.log(b);
+
+// console.log(b);
+
+let obj1={
+    name:"Raju"
+};
+
+// let obj2 =obj1;
+
+// obj2.name="Rahim";
+
+
+// console.log(obj2);
+
+//Undefined
+
+// let x;
+
+// console.log(x);
+
+// function test (){}
+
+// console.log(test());
+
+
+// let num = Number("25");
+
+
+// let name = String(100);
+
+
+// let bol = Boolean(0);
+
+// console.log(bol);
+
+
+// console.log("10" + 5);
+
+// console.log("10" - 5);
+
+// console.log("10" * 5);
+
+
+// console.log("10" / 5);
+
+
+// console.log(true + true);
+
+// console.log(true + false); //1+0=1
+
+// console.log(false + false); //0+0=0
+
+
+console.log(Number("10") +5);
---
+++ day5.js
@@ -0,0 +1,44 @@
+// let name1 ="Dhaka";
+// let name2 ='Rangpur';
+
+// console.log(name1);
+// console.log(name1.length);
+
+// let name ="Raju";
+// let age =26;
+
+// console.log(` My name is ${name} and age is ${age}`);
+
+// let msg =`Hello
+// How are you?`;
+
+// console.log(msg);
+
+// let text ="Hello";
+// console.log(text.toUpperCase());
+// console.log(text.toLowerCase());
+
+// let text ="  Java Script   ";
+
+// console.log(text.slice(0,4));
+
+// console.log(text.replace("Java","Js"));
+// console.log(text);
+
+// let text ="a,b,c,d ";
+
+// console.log(text.split(","));
+
+// let text = "Hello";
+// console.log(text.includes("all"));
+
+let text = "Hello World";
+
+// console.log(text.indexOf("World"));
+
+// console.log(text.lastIndexOf("World"));
+
+// console.log("Hello Hello ".lastIndexOf("Hello"));
+// console.log("Hello Hello ".endsWith("lo"));
+
+console.log("5" +5);
