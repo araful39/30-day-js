3c7472a7488f0786335a64f71799267d8a2014d4
araful <rajuslam39@gmail.com>
2026-04-26 12:47:57 +0600

    day7 js array and all build in methods done

---
+++ day7.js
@@ -0,0 +1,63 @@
+//Array
+
+
+
+let  fruits = ["apple","banana", "mango"];
+
+
+// console.log(fruits);
+
+// console.log(fruits[0]);
+// console.log(fruits[1]);
+// console.log(fruits[2]);
+
+// console.log(fruits[5]);
+
+
+// let num =[true, false, 1,2, "A", "B",2.3];
+
+// console.log(num);
+
+// let nums =[1,2,3,4,5,6,7,8,9];
+
+// num.push(10);
+
+// num.pop();
+
+// num.unshift(20);
+
+// num.shift();
+
+
+// let arr =[1,2,5,6,7,8,9];
+
+// nums.forEach(e => {
+// console.log(e);   
+// });
+// let arr= nums.map(e => e* 2) ;
+
+// console.log(arr);let 
+// let even =  nums.filter(e => e %2===0); 
+// console.log(even);
+
+// let  sum = nums.reduce((a, b) => a +b, 0);
+
+// console.log(sum);
+
+// let even = arr.includes(20);
+
+// console.log(even);
+// let result =  arr.find(e => e >2);
+
+
+let arr =[100,2,50,16,70,8,9];
+
+//   let newArr= arr.sort((a, b)=> a -b);
+
+ let newArr = arr.reverse();
+console.log(newArr);
+
+
+
+
+
